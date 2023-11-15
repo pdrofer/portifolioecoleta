2cd818ce8da01c8e1f11e1f062552ff25ff4f2ba
“Pedro <pedrorofer@gmail.com>
2023-11-15 01:03:12 -0300

    portfolio check

---
+++ NLWcopy/public/scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+// document.write("hello")
+
+//variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso e uma string"
+// 'Isso tambem e uma string'
+// `Isso e uma string tambem`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+//boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propiedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+ // Array - Vetores(Colecao de dados)
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+
+// registrar funcao
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Pedro")
+// sayMyName("Kyan")
+// sayMyName("Valeska")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop - repeticoes
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
